a9a944c22d5bbdddb35aa1ca4487198c17f2c09e
katrienvermiert <katrien.vermiert@gmail.com>
2019-08-21 12:08:54 +0200

    finished

--- script.js
+++ script.js
@@ -5,3 +5,16 @@
         $('.nav-list').toggleClass('open');
     });
 });
+
+function show() {
+  let more = document.getElementById("more");
+  let button = document.getElementById("button");
+
+  if(more.style.display === 'none'){
+    more.style.display = 'block';
+    button.innerHTML ='See less';
+  } else {
+    more.style.display = 'none';
+    button.innerHTML = 'See more'
+  }
+}
